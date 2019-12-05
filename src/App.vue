96472996313d0e324bf2212c160b163bd72a96e9
LazyTechwork <lazytechwork@gmail.com>
2019-12-05 11:22:57 +0300

    Some fixes and simplifying things

--- src/App.vue
+++ src/App.vue
@@ -24,14 +24,6 @@
 <style>
     .bg-greeny {
         background: linear-gradient(45deg, rgba(0,175,102,1) 0%, rgba(0,195,137,1) 100%);
-        animation: design-bg 8s ease infinite;
-        background-size: 150% 150%;
-    }
-
-    @keyframes design-bg {
-        0% {background-position: 0}
-        50% {background-position: 100%}
-        100% {background-position: 0}
     }
 
 </style>
