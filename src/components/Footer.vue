79ac3c344130e366d094cc2f08254c9bdfe0a46e
LazyTechwork <lazytechwork@gmail.com>
2019-12-05 11:15:30 +0300

    Ya.Metrika added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
     <div class="text-white bg-greeny">
         <p class="container my-3 text-center">Copyright <a href="//vk.com/ipetrovofficial" class="text-white font-weight-bold">LazyTechwork</a> &amp; <a
                 href="//vk.com/tekly" class="text-white font-weight-bold">Tekly Technologies</a> &copy; 2019</p>
+        <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(56533735, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/56533735" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
     </div>
 </template>
 
